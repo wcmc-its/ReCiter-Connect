efb3434985ed88a89fd3d633faf15f7e3ca8b0fd
Sarbajit Dutta <sarbajitdutta35@gmail.com>
2020-10-19 11:54:21 -0400

    add ingest type for people

--- src/main/java/org/vivoweb/harvester/ingest/EdDataInterfaceImpl.java
+++ src/main/java/org/vivoweb/harvester/ingest/EdDataInterfaceImpl.java
@@ -1,15 +1,20 @@
 package org.vivoweb.harvester.ingest;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.vivoweb.harvester.util.repo.SDBJenaConnect;
 
 import lombok.extern.slf4j.Slf4j;
 import reciter.connect.database.mysql.jena.JenaConnectionFactory;
+import reciter.connect.vivo.IngestType;
 import reciter.connect.vivo.api.client.VivoClient;
 
 
@@ -24,6 +29,11 @@ public class EdDataInterfaceImpl implements EdDataInterface {
 	@Autowired
 	private VivoClient vivoClient;
 
+	@Autowired
+	private JenaConnectionFactory jcf;
+
+	private String ingestType = System.getenv("INGEST_TYPE");
+
 	public List<String> getPeopleInVivo(JenaConnectionFactory jcf) {
 		
 		List<String> people = new ArrayList<String>();
@@ -37,21 +47,39 @@ public class EdDataInterfaceImpl implements EdDataInterface {
 				"?people rdf:type foaf:Person . \n" +
 				//"FILTER(REGEX(STR(?people),\"bhb9002\")) \n" +
 				"}}";
-		
-		try {
-			String response = vivoClient.vivoQueryApi(sparqlQuery);
-			log.info(response);
-			JSONObject obj = new JSONObject(response);
-			JSONArray bindings = obj.getJSONObject("results").getJSONArray("bindings");
-			if(bindings != null && !bindings.isEmpty()) {
-				for (int i = 0; i < bindings.length(); ++i) {
-					people.add(bindings.getJSONObject(i).getJSONObject("people").getString("value").replace("https://vivo.med.cornell.edu/individual/cwid-", ""));
+
+		if(ingestType.equals(IngestType.VIVO_API.toString())) {
+			try{
+				String response = vivoClient.vivoQueryApi(sparqlQuery);
+				log.info(response);
+				JSONObject obj = new JSONObject(response);
+				JSONArray bindings = obj.getJSONObject("results").getJSONArray("bindings");
+				if(bindings != null && !bindings.isEmpty()) {
+					for (int i = 0; i < bindings.length(); ++i) {
+						people.add(bindings.getJSONObject(i).getJSONObject("people").getString("value").replace(JenaConnectionFactory.nameSpace + "cwid-", ""));
+					}
+				} else {
+					log.info("No result from the query");
 				}
-			} else {
-				log.info("No result from the query");
+			} catch(Exception  e) {
+				log.info("Api Exception", e);
 			}
-		} catch(Exception e) {
-			log.error("Exception", e);
+		} else if(ingestType.equals(IngestType.SDB_DIRECT.toString())) {
+			SDBJenaConnect vivoJena = this.jcf.getConnectionfromPool("dataSet");
+			try {
+				ResultSet rs = vivoJena.executeSelectQuery(sparqlQuery, true);
+				while(rs.hasNext())
+				{
+					QuerySolution qs =rs.nextSolution();
+					if(qs.get("people") != null) {
+						people.add(qs.get("people").toString().replace(JenaConnectionFactory.nameSpace + "cwid-", "").trim());
+					}
+					
+				}
+			} catch(IOException e) {
+				log.error("Error connecting to Jena database", e);
+			}
+			this.jcf.returnConnectionToPool(vivoJena, "dataSet");
 		}
 		return people;
 	}
